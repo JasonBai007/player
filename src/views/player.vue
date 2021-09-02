71a12eb9786d72794adce50634aac2125362ba40
JasonBai007 <byygkcg@163.com>
2021-09-02 16:03:32 +0800

    实现基础功能

--- src/views/player.vue
+++ src/views/player.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="player-wrap">
-    <img src="../assets/logo.png" />
     <h1>《精神焦虑症的自救》</h1>
-    <audio :src="src" controls></audio>
-    <!-- <el-button @click="play">adff</el-button> -->
+    <h6>Claire Weekes</h6>
+    <img src="../assets/hero.jpg" class="hero" />
+    <template v-if="listData.length>0">
+      <div class="list" v-for="(item,i) in listData" :key="i">
+        <h5>{{item.title}}</h5>
+        <audio :src="item.link" controls></audio>
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
 let list = ref([]);
-const i = ref(0);
-const src = computed(() => {
-  console.log(list.value);
-  return "https://lubanseven.gitee.io/nerves/mp3/" + list.value[i.value];
+
+const listData = computed(() => {
+  return list.value.map((name) => {
+    return {
+      title: name,
+      link: `https://lubanseven.gitee.io/nerves/mp3/${name}`,
+    };
+  });
 });
 
 onMounted(() => {
   getList();
 });
-function play() {
-  console.log("play");
-}
+
 function getList() {
   fetch("/nerves/list.json")
     .then((res) => {
@@ -35,18 +42,12 @@ function getList() {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+h1 {
+  margin: 20px 0 20px;
+  font-size: 24px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hero {
+  display: block;
+  margin: 10px auto;
 }
 </style>
